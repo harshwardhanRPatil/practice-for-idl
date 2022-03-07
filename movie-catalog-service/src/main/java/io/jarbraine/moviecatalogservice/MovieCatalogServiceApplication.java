2b3a6ec0e93bfabdcf8b9268e981932b162ae9d2
harsh <harshwardhan@gmail.com>
2022-03-07 22:36:53 +0530

    try confllictest 5

--- movie-catalog-service/src/main/java/io/jarbraine/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/jarbraine/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
+//we do
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 	@Bean
