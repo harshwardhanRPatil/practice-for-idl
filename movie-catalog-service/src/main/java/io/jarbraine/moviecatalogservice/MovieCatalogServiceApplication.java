945f84645004e6bf37e909ae82a3b1f36bd976d4
harshwardhanRPatil <101011035+harshwardhanRPatil@users.noreply.github.com>
2022-03-08 22:26:15 +0530

    Update MovieCatalogServiceApplication.java
    just to create error

--- movie-catalog-service/src/main/java/io/jarbraine/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/jarbraine/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,5 +1,6 @@
 package io.jarbraine.moviecatalogservice;
 //just add gor checking 
+//to create more error 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
